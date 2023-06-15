6189568ecdce49e7403529821216e6aa5f4d5b40
amygourlaymohc <89403492+amygourlaymohc@users.noreply.github.com>
2023-06-15 14:56:31 +0100

    fix: canvas suspense

--- src/general/PhoneCanvas.tsx
+++ src/general/PhoneCanvas.tsx
@@ -6,21 +6,21 @@ const Phone3D = lazy(() => import('src/general/Phone3D'))
 
 const PhoneCanvas: FunctionComponent = () => {
     return (
-        <Suspense fallback={null}>
-            <Canvas
-                camera={{
-                    zoom: 2.2,
-                }}
-                style={{
-                    height: '600px',
-                    width: '380px',
-                    margin: '-24px',
-                }}
-            >
+        <Canvas
+            camera={{
+                zoom: 2.2,
+            }}
+            style={{
+                height: '600px',
+                width: '380px',
+                margin: '-24px',
+            }}
+        >
+            <Suspense fallback={null}>
                 <ambientLight />
                 <Phone3D />
-            </Canvas>
-        </Suspense>
+            </Suspense>
+        </Canvas>
     )
 }
 
