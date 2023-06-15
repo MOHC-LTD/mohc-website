3e87c36acd35a573e61540a517c3cc16fa19967a
amygourlaymohc <89403492+amygourlaymohc@users.noreply.github.com>
2023-06-15 14:22:19 +0100

    fix: use suspense

--- src/general/PhoneCanvas.tsx
+++ src/general/PhoneCanvas.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, lazy } from 'react'
+import { FunctionComponent, lazy, Suspense } from 'react'
 
 import { Canvas } from '@react-three/fiber'
 
@@ -6,19 +6,21 @@ const Phone3D = lazy(() => import('src/general/Phone3D'))
 
 const PhoneCanvas: FunctionComponent = () => {
     return (
-        <Canvas
-            camera={{
-                zoom: 2.2,
-            }}
-            style={{
-                height: '600px',
-                width: '380px',
-                margin: '-24px',
-            }}
-        >
-            <ambientLight />
-            <Phone3D />
-        </Canvas>
+        <Suspense fallback={null}>
+            <Canvas
+                camera={{
+                    zoom: 2.2,
+                }}
+                style={{
+                    height: '600px',
+                    width: '380px',
+                    margin: '-24px',
+                }}
+            >
+                <ambientLight />
+                <Phone3D />
+            </Canvas>
+        </Suspense>
     )
 }
 
